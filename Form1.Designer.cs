1ed952f894fc767ce2835454d7918aed06c1e28c
elifddalda <elifdalda7@gmail.com>
2026-04-17 22:28:28 +0300

    Add files via upload
    C# Windows Forms ile öğrenci takip sistemi projesi.

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace ogrenciTkip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtNo = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(40, 35);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(171, 22);
+            this.txtAd.TabIndex = 0;
+            this.txtAd.TextChanged += new System.EventHandler(this.txtAd_TextChanged);
+            // 
+            // txtNo
+            // 
+            this.txtNo.Location = new System.Drawing.Point(267, 35);
+            this.txtNo.Name = "txtNo";
+            this.txtNo.Size = new System.Drawing.Size(166, 22);
+            this.txtNo.TabIndex = 1;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(321, 93);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(112, 23);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "EKLE";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnListele
+            // 
+            this.btnListele.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnListele.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnListele.Location = new System.Drawing.Point(321, 136);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(112, 23);
+            this.btnListele.TabIndex = 3;
+            this.btnListele.Text = "LİSTELE";
+            this.btnListele.UseVisualStyleBackColor = false;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.Color.Crimson;
+            this.btnSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSil.Location = new System.Drawing.Point(321, 180);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(112, 23);
+            this.btnSil.TabIndex = 4;
+            this.btnSil.Text = "SİL";
+            this.btnSil.UseVisualStyleBackColor = false;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(40, 93);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(241, 180);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LimeGreen;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(321, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "GÜNCELLE";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtNo);
+            this.Controls.Add(this.txtAd);
+            this.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtNo;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ogrenciTkip
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection baglanti = new SqlConnection("Server=localhost\\SQLEXPRESS;Database=OgrenciDB;Trusted_Connection=True;");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            baglanti.Open();
+
+            SqlCommand komut = new SqlCommand("SELECT * FROM Ogrenciler", baglanti);
+            SqlDataReader oku = komut.ExecuteReader();
+
+            while (oku.Read())
+            {
+                listBox1.Items.Add(oku["Ad"] + " - " + oku["Numara"]);
+            }
+
+            baglanti.Close();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            if (txtAd.Text == "" || txtNo.Text == "")
+            {
+                MessageBox.Show("Lütfen tüm alanları doldurun!");
+                return;
+            }
+            baglanti.Open();
+
+            SqlCommand komut = new SqlCommand("INSERT INTO Ogrenciler (Ad, Numara) VALUES (@ad, @no)", baglanti);
+            komut.Parameters.AddWithValue("@ad", txtAd.Text);
+            komut.Parameters.AddWithValue("@no", txtNo.Text);
+
+            komut.ExecuteNonQuery();
+
+            baglanti.Close();
+
+            MessageBox.Show("Veri eklendi");
+            txtAd.Clear();
+            txtNo.Clear();
+            btnListele.PerformClick();
+        }
+        
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            baglanti.Open();
+
+            SqlCommand komut = new SqlCommand("SELECT * FROM Ogrenciler", baglanti);
+            SqlDataReader oku = komut.ExecuteReader();
+
+            while (oku.Read())
+            {
+                listBox1.Items.Add(oku["Ad"] + " - " + oku["Numara"]);
+            }
+
+            baglanti.Close();
+
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string secilen = listBox1.SelectedItem.ToString();
+
+                string ad = secilen.Split('-')[0].Trim();
+                string numara = secilen.Split('-')[1].Trim();
+
+                baglanti.Open();
+
+                SqlCommand komut = new SqlCommand("DELETE FROM Ogrenciler WHERE Ad=@ad AND Numara=@no", baglanti);
+                komut.Parameters.AddWithValue("@ad", ad);
+                komut.Parameters.AddWithValue("@no", numara);
+
+                komut.ExecuteNonQuery();
+
+                baglanti.Close();
+
+                MessageBox.Show("Silindi");
+                listBox1.Items.Clear();
+
+                baglanti.Open();
+
+                SqlCommand komut2 = new SqlCommand("SELECT * FROM Ogrenciler", baglanti);
+                SqlDataReader oku2 = komut2.ExecuteReader();
+
+                while (oku2.Read())
+                {
+                    listBox1.Items.Add(oku2["Ad"] + " - " + oku2["Numara"]);
+                }
+
+                baglanti.Close();
+            }
+        }
+
+       
+        
+            private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            if (listBox1.SelectedItem != null)
+            {
+                string secilen = listBox1.SelectedItem.ToString();
+
+                txtAd.Text = secilen.Split('-')[0].Trim();
+                txtNo.Text = secilen.Split('-')[1].Trim();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+            baglanti.Open();
+
+            SqlCommand komut = new SqlCommand("UPDATE Ogrenciler SET Numara=@no WHERE Ad=@ad", baglanti);
+            komut.Parameters.AddWithValue("@ad", txtAd.Text);
+            komut.Parameters.AddWithValue("@no", txtNo.Text);
+
+            komut.ExecuteNonQuery();
+
+            baglanti.Close();
+
+            MessageBox.Show("Güncellendi");
+
+            btnListele.PerformClick();
+        }
+
+        private void txtAd_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+      
+    
+
+
